8063286a6fb51a853510908d4c675ef7a7d36c9a
Joey Zenhäusern <joeyzenh@gmail.com>
2019-11-16 23:35:56 +0100

    Fix laptop node name

--- .config/i3/build-i3blocks-config.sh
+++ .config/i3/build-i3blocks-config.sh
@@ -3,7 +3,7 @@
 NODE=$(uname -n)
 if [ "$NODE" = "joey-desktop" ]; then
     cat ~/.config/i3blocks/config.base ~/.config/i3blocks/config.desktop > ~/.config/i3blocks/config
-elif [ "$NODE" = "laptop-joey" ]; then
+elif [ "$NODE" = "carbon-joey" ]; then
     cat ~/.config/i3blocks/config.base ~/.config/i3blocks/config.laptop > ~/.config/i3blocks/config
 else
     cat ~/.config/i3blocks/config.base > ~/.config/i3blocks/config
