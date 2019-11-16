5bd0bd3ad5434c3333e6bd5e559f5f41222a397c
Joey Zenhäusern <joeyzenh@gmail.com>
2019-11-16 23:29:03 +0100

    Add i3blocks build config file script

---
+++ .config/i3/build-i3blocks-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NODE=$(uname -n)
+if [ "$NODE" = "joey-desktop" ]; then
+    cat ~/.config/i3blocks/config.base ~/.config/i3blocks/config.desktop > ~/.config/i3blocks/config
+elif [ "$NODE" = "laptop-joey" ]; then
+    cat ~/.config/i3blocks/config.base ~/.config/i3blocks/config.laptop > ~/.config/i3blocks/config
+else
+    cat ~/.config/i3blocks/config.base > ~/.config/i3blocks/config
+fi
